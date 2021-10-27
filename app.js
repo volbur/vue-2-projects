77c58424965e2e6c215ea09b42acbf643f048292
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-10-27 22:14:21 +0300

    Adding all hooks and console.log for them

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ new Vue({
   data: {
     title: 'The VueJS Instance',
   },
-
   methods: {},
   computed: {},
   watch: {},
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  create() {
+    console.log('create()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy()');
+  },
+  destroyed() {
+    console.log('destroyed()');
+  },
 });
